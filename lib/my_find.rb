fdb6759d9b329da9c492fa1c3172aacba7553288
Andrew Zimmer <andrewzimmer.dev@gmail.com>
2020-05-06 18:07:29 +0000

    Done.

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,5 +1,19 @@
 require 'pry'
 
-def my_find(collection)
+collection = (1..100).to_a
 
-end
+
+def my_find(collection)
+  i = 0 
+  while i < collection.length 
+    if yield(collection[i])
+      return collection[i]
+    end 
+    i += 1 
+  end 
+end 
+
+binding.pry 
+my_find(collection) {|i| i % 3 == 0 and i % 5 == 0 }
+
+#why does this stop at the first true instance?
